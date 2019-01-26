7295d861d503aae30e21c8d1555072db20fdf2dd
Abishek Jayathirtha <abishek.jayathirtha@mail.utoronto.ca>
2019-01-26 12:50:01 -0500

    Implemented MSE function

--- starter.py
+++ starter.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linalg as LA
 
 def loadData():
     with np.load('notMNIST.npz') as data :
@@ -19,10 +20,17 @@ def loadData():
         trainData, trainTarget = Data[:3500], Target[:3500]
         validData, validTarget = Data[3500:3600], Target[3500:3600]
         testData, testTarget = Data[3600:], Target[3600:]
+        print traindata
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
 def MSE(W, b, x, y, reg):
-    # Your implementation here
+    size=y.size# Your implementation here
+    y_pred=x.dot(W.transpose()).flatten()
+    error=power((y_pred+b-y.flatten()),2)
+    estimate=(1/size)*error.sum()
+    decay=LA.norm(W)*LA.norm(W)*0.5*reg
+    return (estimate-decay)
+   
 
 def gradMSE(W, b, x, y, reg):
     # Your implementation here
@@ -38,4 +46,5 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
 
 def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
     # Your implementation here
+
 
