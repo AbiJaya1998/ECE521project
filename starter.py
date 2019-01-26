99bc1884845e92af3c1a34a2c199e56082ed71c2
Alex <xingwen.li@mail.utoronto.ca>
2019-01-26 12:50:02 -0500

    Added MSE Error1

--- starter.py
+++ starter.py
@@ -21,21 +21,41 @@ def loadData():
         testData, testTarget = Data[3600:], Target[3600:]
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
-def MSE(W, b, x, y, reg):
-    # Your implementation here
 
-def gradMSE(W, b, x, y, reg):
-    # Your implementation here
+trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+print("Data type: ", type(Data))
+print("Target type: ", type(Target))
 
-def crossEntropyLoss(W, b, x, y, reg):
-    # Your implementation here
 
-def gradCE(W, b, x, y, reg):
-    # Your implementation here
+def MSE(W, b, x, y, reg):  # Your implementation here
+    size=y.size# Your implementation here
+    y_pred=x.dot(W.transpose()).flatten()
+    error=power((y_pred+b-y.flatten()),2)
+    estimate=(1/size)*error.sum()
+    decay=LA.norm(W)*LA.norm(W)*0.5*reg
+    return
 
-def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
-    # Your implementation here
 
-def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
-    # Your implementation here
+def gradMSE(W, b, x, y, reg):  # Your implementation here
+
+    return
+
+
+def crossEntropyLoss(W, b, x, y, reg):  # Your implementation here
+
+    return
+
+
+def gradCE(W, b, x, y, reg): # Your implementation here
+
+    return
+
+
+def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS): # Your implementation here
+
+    return
+
+
+def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None): # Your implementation here
+    return
 
