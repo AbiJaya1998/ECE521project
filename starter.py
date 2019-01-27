1bf465e09e791b62b57197069df1bdeea62d6294
Abishek Jayathirtha <abishek.jayathirtha@mail.utoronto.ca>
2019-01-26 19:37:59 -0500

    Part 1 Assignment 1 mostly Done

--- starter.py
+++ starter.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 
+img_h = img_w = 28             # MNIST images are 28x28
+img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
+
+ 
 def loadData():
     with np.load('notMNIST.npz') as data :
         Data, Target = data ['images'], data['labels']
@@ -20,22 +24,80 @@ def loadData():
         trainData, trainTarget = Data[:3500], Target[:3500]
         validData, validTarget = Data[3500:3600], Target[3500:3600]
         testData, testTarget = Data[3600:], Target[3600:]
-        print traindata
+        
     return trainData, validData, testData, trainTarget, validTarget, testTarget
+
+
+def preproc(trainData, validData=None, testData=None):
+
+	symm_train = np.zeros((len(trainData), 2))
+	for i in range(0, len(trainData)):
+		flipped_train = np.flipud(trainData[i])
+		square_diff_train = np.square(trainData[i] - flipped_train).sum()
+		symm_train[i]=np.array([1, square_diff_train])
+	
+	returned_vals = (symm_train)
+
+	if validData is not None and testData is not None:
+		symm_valid = np.zeros((len(validData), 2))
+		symm_test = np.zeros((len(testData), 2))
+
+		for i in range(0, len(validData)):
+			flipped_valid = np.flipud(validData[i])
+			square_diff_valid = np.square(validData[i] - flipped_valid).sum()
+			symm_valid[i] = np.array([1, square_diff_valid])
+
+		for j in range(0, len(testData)):
+			flipped_test = np.flipud(testData[i])
+			square_diff_test = np.square(testData[i] - flipped_test).sum()
+			symm_test[i] = np.array([1, square_diff_test])
+		
+		returned_vals = (symm_train, symm_valid, symm_test)
+	
+	return returned_vals
+
 
 def MSE(W, b, x, y, reg):
     size=y.size# Your implementation here
     y_pred=x.dot(W.transpose()).flatten()
-    error=power((y_pred+b-y.flatten()),2)
-    estimate=(1/size)*error.sum()
+    error=np.power((y_pred+b-y.flatten()),2)
+    estimate=(1/2*size)*error.sum()
     decay=LA.norm(W)*LA.norm(W)*0.5*reg
     return (estimate-decay)
    
 
 def gradMSE(W, b, x, y, reg):
     # Your implementation here
+     size=y.size
+     y_pred=x.dot(W.transpose()).flatten()
+     error = (y_pred + b - y.flatten()) + reg*W[:-1].sum()
+     return (error.dot(x)/size)
 
-def crossEntropyLoss(W, b, x, y, reg):
+
+def grad_loop(W, b, trainingData,trainingLabels, reg,alpha):
+    error=gradMSE(W,b,trainingData,trainingLabels,reg)
+    
+    norm=np.linalg.norm(error) 
+    error/=norm
+
+    #print W
+    W+=-(error)*(alpha)
+    return norm,W
+
+
+    # Your implementation here
+    #E=MSE(W,b,trainingData,trainingLabels,reg) if close to zero don';t run gradient descent
+def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
+   
+    while iterations<1000000:
+        g_val,W=grad_loop(W, b, trainingData,trainingLabels, reg,alpha)
+        iterations+=1
+        print("W: ", W)
+    	if g_val<0.001:
+             print"done"
+             break
+    print("W: ", W)
+'''def crossEntropyLoss(W, b, x, y, reg):
     # Your implementation here
 
 def gradCE(W, b, x, y, reg):
@@ -46,5 +108,21 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
 
 def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
     # Your implementation here
+'''
+
+trainData, validData, testData, trainTarget, validTarget, testTarget=loadData()
+all_data = preproc(trainData, validData, testData)
+trainData = all_data[0]
+validData = all_data[1]
+testData = all_data[2]
+
+W = np.linspace(0.0, 1.0, num=2)
+atStart = MSE(W, 0, trainData, trainTarget, reg=0.5)
+
+grad_descent(W, 0 ,trainData,trainTarget,alpha=0.005,iterations=0.0,reg=0.5,EPS=1e-7)
+
+atEnd = MSE(W, 0, trainData, trainTarget, reg=0.5)
+print(atStart,atEnd)
+
 
 
