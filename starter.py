eec1b004c1cd43c367d3e0f25d5635dbc1ccf2d7
Alex <xingwen.li@mail.utoronto.ca>
2019-01-27 18:21:51 -0500

    Pycharm Things added for Me

--- starter.py
+++ starter.py
@@ -1,10 +1,12 @@
-import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def loadData():
     with np.load('notMNIST.npz') as data :
         Data, Target = data ['images'], data['labels']
+        print(Data[0].shape)
+        print(Target[len(Target) - 1])
         posClass = 2
         negClass = 9
         dataIndx = (Target==posClass) + (Target==negClass)
@@ -21,20 +23,15 @@ def loadData():
         testData, testTarget = Data[3600:], Target[3600:]
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
-
-trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
-print("Data type: ", type(Data))
-print("Target type: ", type(Target))
-
-
+"""
 def MSE(W, b, x, y, reg):  # Your implementation here
     size=y.size# Your implementation here
     y_pred=x.dot(W.transpose()).flatten()
-    error=power((y_pred+b-y.flatten()),2)
+    error=np.power((y_pred+b-y.flatten()),2)
     estimate=(1/size)*error.sum()
     decay=LA.norm(W)*LA.norm(W)*0.5*reg
     return
-
+"""
 
 def gradMSE(W, b, x, y, reg):  # Your implementation here
 
@@ -59,3 +56,4 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
 def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None): # Your implementation here
     return
 
+trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
