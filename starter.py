44e83c827481f66ffc02c00e93327532204ecb48
Abishek Jayathirtha <abishek.jayathirtha@mail.utoronto.ca>
2019-01-26 20:11:50 -0500

    Part 1 Assignment 1 Report Left

--- starter.py
+++ starter.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
+from numpy.linalg import inv
 
 img_h = img_w = 28             # MNIST images are 28x28
 img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
@@ -33,7 +34,7 @@ def preproc(trainData, validData=None, testData=None):
 	symm_train = np.zeros((len(trainData), 2))
 	for i in range(0, len(trainData)):
 		flipped_train = np.flipud(trainData[i])
-		square_diff_train = np.absolute(trainData[i] - flipped_train).sum()
+		square_diff_train = np.square(trainData[i] - flipped_train).sum()
 		symm_train[i]=np.array([1, square_diff_train])
 	
 	returned_vals = (symm_train)
@@ -44,12 +45,12 @@ def preproc(trainData, validData=None, testData=None):
 
 		for i in range(0, len(validData)):
 			flipped_valid = np.flipud(validData[i])
-			square_diff_valid = np.absolute(validData[i] - flipped_valid).sum()
+			square_diff_valid = np.square(validData[i] - flipped_valid).sum()
 			symm_valid[i] = np.array([1, square_diff_valid])
 
 		for j in range(0, len(testData)):
 			flipped_test = np.flipud(testData[i])
-			square_diff_test = np.absolute(testData[i] - flipped_test).sum()
+			square_diff_test = np.square(testData[i] - flipped_test).sum()
 			symm_test[i] = np.array([1, square_diff_test])
 		
 		returned_vals = (symm_train, symm_valid, symm_test)
@@ -61,9 +62,12 @@ def MSE(W, b, x, y, reg):
     size=y.size# Your implementation here
     y_pred=x.dot(W.transpose()).flatten()
     error=np.power((y_pred+b-y.flatten()),2)
-    estimate=(1/2*size)*error.sum()
+    print error.sum()
+    print size
+    estimate=error.sum()/(2*size)
+    print estimate
     decay=LA.norm(W)*LA.norm(W)*0.5*reg
-    return (estimate-decay)
+    return (estimate + decay)
    
 
 def gradMSE(W, b, x, y, reg):
@@ -89,7 +93,7 @@ def grad_loop(W, b, trainingData,trainingLabels, reg,alpha):
     #E=MSE(W,b,trainingData,trainingLabels,reg) if close to zero don';t run gradient descent
 def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
    
-    while iterations<1000000:
+    while iterations<100000:
         g_val,W=grad_loop(W, b, trainingData,trainingLabels, reg,alpha)
         iterations+=1
         print("W: ", W)
@@ -117,12 +121,12 @@ validData = all_data[1]
 testData = all_data[2]
 
 W = np.linspace(0.0, 1.0, num=2)
-atStart = MSE(W, 0, trainData, trainTarget, reg=0.5)
+atStart = MSE(W, 0, trainData, trainTarget, reg=0.0)
 
-grad_descent(W, 0 ,trainData,trainTarget,alpha=0.005,iterations=0.0,reg=0.5,EPS=1e-7)
+grad_descent(W, 0 ,trainData,trainTarget,alpha=0.0003,iterations=0.0,reg=0.0,EPS=1e-7)
 
-atEnd = MSE(W, 0, trainData, trainTarget, reg=0.5)
+atEnd = MSE(W, 0, trainData, trainTarget, reg=0.0)
 print(atStart,atEnd)
-
+print "W",np.matmul(np.matmul(inv(np.matmul(trainData.transpose(),trainData)),trainData.transpose()),trainTarget)
 
 
